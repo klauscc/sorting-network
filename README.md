5950c6b40fe6ef6737a9930d861d4773f0d02f46
klauscc <379749641@qq.com>
2017-03-05 18:31:36 +0800

    extended to N=2^k

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # sorting-network
 Python script to check sorting networks and generate sorting network diagrams
+This work is an extension of [https://github.com/brianpursley/sorting-network](https://github.com/brianpursley/sorting-network), which only has implemented the network diagrams of 4,5 and must have a connection network file supplied.
+My work is to automatically generate the connetion network of N=2^k and improve the original drawing logic which is not capable to extend to other N.
 
 ## Usage
 
@@ -8,69 +10,39 @@ usage: sortingnetwork.py [-h] [-i inputfile] [-o [outputfile]] [-c] [-s [list]] 
 
 optional arguments:
   -h, --help                                show this help message and exit
-  -i inputfile, --input inputfile           specify a file containing comparison network definition
-  -o [outputfile], --output [outputfile]    specify a file for saving the comparison network definition
-  -c, --check                               check whether it is a sorting network
+  -i n, --input n           specify the size of network. where n=2^k only
   -s [list], --sort [list]                  sorts the list using the input comparison network
   --svg [outputfile]                        generate SVG
 
-```
-
-Comparison networks can be specified like this: `[[0,1],[2,3],[0,2],[1,3],[1,2]]` and can either be loaded from a file using the `--input` argument or if no input file is specified, read from stdin.
-
-Multiple lines can be used as well, to logically group the comparators at each depth. `[[0,1],[2,3],[0,2],[1,3],[1,2]]` is the same as this:
-```
-[[0,1],[2,3]]
-[[0,2],[1,3]]
-[[1,2]]
-```
-
 ## Examples
-##### Read a comparison network from a file called example.cn and check whether it is is a sorting network.
-```
-./sortingnetwork.py --input example.cn --check
-```
 
-##### Pipe a comparison network from stdin and check whether it is is a sorting network.
-```
-echo "[[0,1],[2,3],[0,2],[1,3],[1,2]]" | ./sortingnetwork.py --check
-```
 
 ##### Read a comparison network from a file called example.cn and generate SVG to stdout.
 ```
-./sortingnetwork.py --input example.cn --svg
+./sortingnetwork.py --input 16 --svg
 ```
 
 ##### Read a comparison network from a file called example.cn and generate SVG, saved to a file called output.svg.
 ```
-./sortingnetwork.py --input example.cn --svg output.svg
+./sortingnetwork.py --input 16 --svg output.svg
 ```
 
 ##### Pipe the output to rsvg-convert to generate a PNG (or other format) instead of SVG.
 (*rsvg-convert can be installed by using `sudo apt-get install librsvg2-bin` on Ubuntu.*)
 
-4-input sorting network:
 ```
-./sortingnetwork.py --input examples/4-input.cn --svg | rsvg-convert > examples/4-input.png
+./sortingnetwork.py --input 16 --svg | rsvg-convert > examples/16-input.png
 ```
-![4-Input Sorting Network](https://github.com/brianpursley/sorting-network/blob/master/examples/4-input.png)
 
-5-input sorting network:
-```
-./sortingnetwork.py --input examples/5-input.cn --svg | rsvg-convert > examples/5-input.png
-```
-![5-Input Sorting Network](https://github.com/brianpursley/sorting-network/blob/master/examples/5-input.png)
+##### Use a specified sorting network to sort a list.**The swap operation of the same layer haven't been parralled!**
 
-##### Use a specified sorting network to sort a list.
-
-Using a 4-input sorting network to sort a list containing 4 items:
 ```
-./sortingnetwork.py --input examples/4-input.cn --sort [2,4,1,3]
+./sortingnetwork.py --input 4 --sort [2,4,1,3]
 ```
 Outputs `[1, 2, 3, 4]`
 
-Using a 5-input sorting network to sort a list containing 5 items:
+Using a 16-input sorting network to sort a list containing 16 items:
 ```
-./sortingnetwork.py --input examples/5-input.cn --sort [5,2,4,1,3]
+./sortingnetwork.py --input 16 --sort [4,2,7,2,5,8,12,5,2,4,1,3,19,21,20,29]
 ```
-Outputs `[1, 2, 3, 4, 5]`
+Outputs `[1, 2, 2, 2, 3, 4, 4, 5, 5, 7, 8, 12, 19, 20, 21, 29]`
--- sortingnetwork.py
+++ sortingnetwork.py
@@ -26,144 +26,171 @@ import sys, argparse
 
 class ComparisonNetwork(list):
 
-	def __str__(self):
-		result = ""
-		usedInputs = []
-		current = []
-		for c in self:
-			if c[0] in usedInputs or c[1] in usedInputs:
-				result += str(current).replace(" ", "") + "\n"
-				del usedInputs[:]
-				del current[:]
-			current.append(c)
-			usedInputs.append(c[0])
-			usedInputs.append(c[1])
-		result += str(current).replace(" ", "")
-		return result
-		
-	def sortBinarySequence(self, sequence):
-		result = sequence
-		for c in self:
-			if (result >> c[0]) & 1 < (result >> c[1]) & 1:
-				result = (result - 2**c[1]) | 2**c[0]
-		return result
-		
-	def sortSequence(self, sequence):
-		result = list(sequence)
-		for c in self:
-			if result[c[0]] > result[c[1]]:
-				result[c[0]], result[c[1]] = result[c[1]], result[c[0]]
-		return result
-		
-	def getMaxInput(self):
-		max = 0
-		for c in self:
-			if c[0] > max:
-				max= c[0]
-			if c[1] > max:
-				max = c[1]
-		return max
-		
-	def svg(self):
-		scale = 1
-		xscale = scale * 35
-		yscale = scale * 20
-		
-		innerResult = ''
-		x = xscale
-		usedInputs = []
-		for c in self:
-			if c[0] in usedInputs or c[1] in usedInputs:
-				x += xscale
-				del usedInputs[:]
-			for ui in usedInputs:
-				if (ui > c[0] and ui < c[1]) or (ui > c[1] and ui < c[0]):
-					x += xscale / 3
-					break
-			y0 = yscale + c[0] * yscale
-			y1 = yscale + c[1] * yscale
-			innerResult += "<circle cx='%s' cy='%s' r='%s' style='stroke:black;stroke-width:1;fill=yellow' />"%(x, y0, 3)
-			innerResult += "<line x1='%s' y1='%s' x2='%s' y2='%s' style='stroke:black;stroke-width:%s' />"%(x, y0, x, y1, 1)
-			innerResult += "<circle cx='%s' cy='%s' r='%s' style='stroke:black;stroke-width:1;fill=yellow' />"%(x, y1, 3)
-			usedInputs.append(c[0])
-			usedInputs.append(c[1])
-		
-		w = x + xscale
-		n = self.getMaxInput() + 1
-		h = (n + 1) * yscale
-		result = "<?xml version='1.0' encoding='utf-8'?>"
-		result += "<!DOCTYPE svg>"
-		result += "<svg width='%spx' height='%spx' xmlns='http://www.w3.org/2000/svg'>"%(w, h)
-		for i in range(0, n):
-			y = yscale + i * yscale
-			result += "<line x1='%s' y1='%s' x2='%s' y2='%s' style='stroke:black;stroke-width:%s' />"%(0, y, w, y, 1)
-		result += innerResult
-		result += "</svg>"
-		return result
-		
-class SortingNetworkChecker:
-	def __init__(self, numberOfInputs):
-		self.numberOfInputs = numberOfInputs
-		self.sortedBinarySequences = []
-		self.maxSequenceToCheck = 2**numberOfInputs
-		for i in range(0, numberOfInputs + 1):
-			bits = "0" * i + "1" * (numberOfInputs - i)
-			self.sortedBinarySequences.append(int(bits, 2))
-			
-	def isSortingNetwork(self, cn):
-		for i in range(1, self.maxSequenceToCheck):
-			if cn.sortBinarySequence(i) not in self.sortedBinarySequences:
-				return False 
-		return True
-		
+    def sortBinarySequence(self, sequence):
+        result = sequence
+        for l in self:
+            for c in l:
+                if (result >> c[0]) & 1 < (result >> c[1]) & 1:
+                    result = (result - 2**c[1]) | 2**c[0]
+        return result
+        
+    def sortSequence(self, sequence):
+        result = list(sequence)
+        for l in self:
+            for c in l:
+                if result[c[0]] > result[c[1]]:
+                    result[c[0]], result[c[1]] = result[c[1]], result[c[0]]
+        return result
+
+
+    #ugly implemention of drawing the network
+    def svg(self, n):
+        scale = 1
+        xscale = scale * 35
+        yscale = scale * 20
+        
+        innerResult = ''
+        x = xscale
+        for l in self:
+            x += xscale
+            usedInputs = []
+            sa = 0
+            hflag = 1
+            for c in l:
+                for ui in usedInputs:
+                    if ui != []:
+                        if ((c[0] > ui[0] and c[0] < ui[1] ) or (c[1] > ui[0] and c[1] < ui[1] )):
+                            sa += 1
+                            x += xscale / 3
+                            hflag = 0
+                            break
+                        if (c[0] < ui[1]):
+                            hflag = 0
+                
+                if usedInputs !=[] and hflag == 1 and sa > 0:
+                    x -= sa * xscale /3
+                    hflag = 0
+                    sa = 0
+
+                y0 = yscale + c[0] * yscale
+                y1 = yscale + c[1] * yscale
+                innerResult += "<circle cx='%s' cy='%s' r='%s' style='stroke:black;stroke-width:1;fill=yellow' />"%(x, y0, 3)
+                innerResult += "<line x1='%s' y1='%s' x2='%s' y2='%s' style='stroke:black;stroke-width:%s' />"%(x, y0, x, y1, 1)
+                innerResult += "<circle cx='%s' cy='%s' r='%s' style='stroke:black;stroke-width:1;fill=yellow' />"%(x, y1, 3)
+                hflag = 1
+                usedInputs.append(c)
+            
+        w = x + xscale
+        h = (n + 1) * yscale
+        result = "<?xml version='1.0' encoding='utf-8'?>"
+        result += "<!DOCTYPE svg>"
+        result += "<svg viewBox='0 0 %s %s' style='background: white' xmlns='http://www.w3.org/2000/svg'>"%(w, h)
+        for i in range(0, n):
+            y = yscale + i * yscale
+            result += "<line x1='%s' y1='%s' x2='%s' y2='%s' style='stroke:black;stroke-width:%s' />"%(0, y, w, y, 1)
+        result += innerResult
+        result += "</svg>"
+        return result
+
+class SortingNetwork():
+    def __init__(self, n):
+        self.checkN(n)
+        self.n = n
+
+    #keep n = 2^k
+    def checkN(self, n):
+        while n > 1:
+            assert n % 2 == 0, "suppose n = 2^k"
+            n /= 2
+
+    def half_cleaner(self, start, n):
+        hc = []
+        assert n % 2 ==0, "for a half cleaner, n should be an even"
+        for i in range(0, n/2):
+            hc += [[i+start,i+start+n /2]]
+        return [hc]
+
+    def bitonic_sorter(self, start, n):
+        if n == 2:
+            bs  = self.half_cleaner(start,n)
+            return bs
+        bs = []
+        bs += self.half_cleaner(start, n)
+        bs1 = self.bitonic_sorter(start, n/2)
+        bs2 = self.bitonic_sorter(start + n/2, n/2)
+        bs += self.merge2list(bs1,bs2)
+        return bs
+
+    def merge2list(self, l1,l2):
+        assert len(l1) == len(l2), "merged two list should have the same length"
+        for i in range(0, len(l1)):
+            l1[i] += l2[i]
+        return l1
+
+    def merger(self, start, n):
+        m = []
+        hc = []
+        for i in range(0,n/2):
+            hc += [[i+start, start+n-i-1]]
+        m += [hc]
+        bs1 = self.bitonic_sorter(start,n/2)
+        bs2 = self.bitonic_sorter(start+n/2,n/2)
+        m += self.merge2list(bs1,bs2)
+        return m
+
+    def sorter(self,start,n):
+        if n == 2:
+            return [[[start,start + 1]]]
+        s = []
+        s1 = self.sorter(start,n/2)
+        s2 = self.sorter(start+n/2,n/2)
+        s += self.merge2list(s1,s2)
+        s += self.merger(start,n)
+        return s
+
+    def sortingnetwork(self):
+        return self.sorter(0, self.n)
+        
 def readComparisonNetwork(filename):
-	cn = ComparisonNetwork()
-	if filename:
-		with open(filename, 'r') as f:
-			for line in f:
-				cn += eval(line)
-	else:
-		for line in sys.stdin:
-			cn += eval(line)
-	return cn
-		
+    cn = ComparisonNetwork()
+    if filename:
+        with open(filename, 'r') as f:
+            for line in f:
+                cn += eval(line)
+    else:
+        for line in sys.stdin:
+            cn += eval(line)
+    return cn
+
+def generateComparisonNetwork(lists):
+    cn = ComparisonNetwork(lists)
+    return cn
+
+        
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-i", "--input", metavar="inputfile", help="specify a file containing comparison network definition")
-	parser.add_argument("-o", "--output", metavar="outputfile", nargs='?', const='', help="specify a file for saving the comparison network definition")
-	parser.add_argument("-c", "--check", action="store_true", help="check whether it is a sorting network")
-	parser.add_argument("-s", "--sort", metavar="list", nargs='?', const='', help="sorts the list using the input comparison network")
-	parser.add_argument("--svg", metavar="outputfile", nargs='?', const='', help="generate SVG")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", metavar="N", type=int, help="specify a file containing comparison network definition")
+    parser.add_argument("-s", "--sort", metavar="list", nargs='?', const='', help="sorts the list using the input comparison network")
+    parser.add_argument("--svg", metavar="outputfile", nargs='?', const='', help="generate SVG")
+    args = parser.parse_args()
 
-	if args.check:
-		cn = readComparisonNetwork(args.input)
-		checker = SortingNetworkChecker(cn.getMaxInput() + 1)
-		print checker.isSortingNetwork(cn)
+    n = args.input
+    sn = SortingNetwork(n)
+    cn = generateComparisonNetwork(sn.sortingnetwork())
+    
+    if args.svg or args.svg == "":
+        if args.svg == "":
+            print cn.svg(n)
+        else:
+            with open(args.svg, "w") as f:
+                f.write(cn.svg(n))
 
-	if args.svg or args.svg == "":
-		cn = readComparisonNetwork(args.input)
-		if args.svg == "":
-			print cn.svg()
-		else:
-			with open(args.svg, "w") as f:
-				f.write(cn.svg())
-
-	if args.output or args.output == "":
-		cn = readComparisonNetwork(args.input)
-		if args.output == "":
-			print str(cn)
-		else:
-			with open(args.output, "w") as f:
-				f.write(str(cn))
-
-	if args.sort or args.sort == "":
-		cn = readComparisonNetwork(args.input)
-		if args.sort == "":
-			inputSequence = eval(sys.stdin.readline())
-		else:
-			inputSequence = eval(args.sort)
-		print cn.sortSequence(inputSequence)
-		
+    if args.sort or args.sort == "":
+        if args.sort == "":
+            inputSequence = eval(sys.stdin.readline())
+        else:
+            inputSequence = eval(args.sort)
+        print cn.sortSequence(inputSequence)
+        
 if __name__ == "__main__":
     main()
